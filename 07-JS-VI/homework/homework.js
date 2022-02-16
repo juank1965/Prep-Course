3fa6f4fae1e681169ddf6d1a182e1a4260dff3de
Juank1965 <juancarlosbastidasq@gmail.com>
2022-02-16 08:39:20 -0500

    Homework 07-JS-VI Optimizando codigo ejercicio filter

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -56,13 +56,20 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  /*
   let nuevoArray = [];
   for (i=0; i < array.length; i++){
     if (array[i][0] === "a"){
       nuevoArray.push(array[i])
     }
   }
-  return nuevoArray
+  return nuevoArray*/
+  let elementosQueComienzanConA = array.filter(function(elemento){
+    return elemento[0]=== "a"
+  }).map(function(elemento){
+    return elemento
+  })
+  return elementosQueComienzanConA
 }
 
 // No modificar nada debajo de esta línea
